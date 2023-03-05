21a6a49d1e1ec69d527c243f3bf2a8fa1352405a
Daniel Volchek <danielvolchek123@gmail.com>
2023-03-05 09:33:39 -0500

    added tests

--- src/tests/models.test.js
+++ src/tests/models.test.js
@@ -48,4 +48,59 @@ describe("Database Model Tests", () => {
     expect(foundCheese.title).toBe("gouda");
     expect(foundCheese.description).toBe("its very gouda");
   });
+
+  describe("tests associations", () => {
+    it("user should be able to have many boards", async () => {
+      const user = await User.create({
+        name: "Test User",
+        email: "user@user.com",
+      });
+
+      const board1 = await Board.create({
+        type: "gouda board",
+        description: "its very gouda board",
+        rating: 10,
+      });
+      const board2 = await Board.create({
+        type: "gouda board 2",
+        description: "its very gouda board 2",
+        rating: 1,
+      });
+
+      user.addBoard(board1);
+      user.addBoard(board2);
+
+      const usersBoards = user.getBoards();
+      expect(Array.isArray(usersBoards)).toBeTrue();
+      expect(usersBoards.length).toBe(2);
+
+      expect(usersBoards[0].id).toBe(board1.id);
+      expect(usersBoards[1].id).toBe(board2.id);
+    });
+
+    it("boards should be able to have many cheeses", async () => {
+      const board = await Board.create({
+        type: "gouda board",
+        description: "its very gouda board",
+        rating: 10,
+      });
+
+      const cheese1 = await Cheese.create({
+        title: "gouda",
+        description: "its very gouda",
+      });
+      const cheese2 = await Cheese.create({
+        title: "gouda",
+        description: "its very gouda",
+      });
+      board.addCheese(cheese1);
+      board.addCheese(cheese2);
+    });
+    it("cheeses should be able to have many boards", async () => {
+      const cheese = await Cheese.create({
+        title: "gouda",
+        description: "its very gouda",
+      });
+    });
+  });
 });
