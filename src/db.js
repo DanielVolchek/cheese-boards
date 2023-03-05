22d19570d1ac4755e2aa75b199955c9cfaba7b27
Daniel Volchek <danielvolchek123@gmail.com>
2023-03-05 09:33:44 -0500

    added db conn

--- src/db.js
+++ src/db.js
@@ -2,15 +2,25 @@
 const Sequelize = require("sequelize");
 const User = require("./models/User");
 const Board = require("./models/Board");
+const Cheese = require("./models/cheese");
 
 const sequelize = new Sequelize({
   dialect: "sqlite",
   storage: "./cheeseboard_db.sqlite",
 });
 
+// associations
+// user -> board - one to many
+Board.belongsTo(User);
+User.hasMany(Board);
+// cheese -> board - many to many
+Board.belongsToMany(Cheese, { through: "cheese_board" });
+Cheese.belongsToMany(Board, { through: "cheese_board" });
+
 module.exports = {
   sequelize,
   Sequelize,
   User,
   Board,
+  Cheese,
 };
